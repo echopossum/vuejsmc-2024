067f6b6b2279a7ad0cf7a4919d25325e34444c5e
Echo <echopossum@gmail.com>
2026-03-01 10:14:08 -0500

    Complete Lesson 42

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
-import { ref } from 'vue'
+import { ref, h } from 'vue'
 import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
+
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
 
 const tasks = ref<Tables<'tasks'>[] | null>()
 ;(async () => {
@@ -11,16 +20,39 @@ const tasks = ref<Tables<'tasks'>[] | null>()
   tasks.value = data
   console.log('tasks: ', tasks)
 })()
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  },
+]
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  },
+]
 </script>
 
 <template>
-  <div>
-    <h1 class="text-red-400">Tasks Page</h1>
-    <RouterLink to="/">Home</RouterLink>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">
-        {{ task.name }}
-      </li>
-    </ul>
-  </div>
+  <DataTable :columns="columns" :data="payments" />
 </template>
